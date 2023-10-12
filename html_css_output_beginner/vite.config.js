538d9bb7f18612ca4ceae7a9e83a364a868d4b16
LeafField <109466326+LeafField@users.noreply.github.com>
2023-10-12 22:53:52 +0900

    初期設定の完了

---
+++ html_css_output_beginner/vite.config.js
@@ -0,0 +1,27 @@
+import { resolve } from "path";
+
+/** @type {import("vite").UserConfig} */
+export default {
+  base: "./",
+  root: "./src",
+  build: {
+    outDir: "../docs",
+    rollupOptions: {
+      output: {
+        assetFileNames: (info) => {
+          console.log(info.name);
+          if (info.name.split(".")[1] === "css") {
+            return "css/style.css";
+          }
+          if (info.name.split(".")[1] === "html") {
+            return "[name][extname]";
+          }
+        },
+      },
+      input: {
+        index: resolve(__dirname, "./src/index.html"),
+        test: resolve(__dirname, "./src/test.html"),
+      },
+    },
+  },
+};
