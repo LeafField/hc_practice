05eb907898bf96eb1a95cdf825e063d90ed1e7c7
LeafField <109466326+LeafField@users.noreply.github.com>
2023-10-13 23:39:29 +0900

    完成しました

--- html_css_output_beginner/vite.config.js
+++ html_css_output_beginner/vite.config.js
@@ -3,23 +3,27 @@ import { resolve } from "path";
 /** @type {import("vite").UserConfig} */
 export default {
   base: "./",
-  root: "./src",
   build: {
     outDir: "../docs",
     rollupOptions: {
       output: {
         assetFileNames: (info) => {
-          console.log(info.name);
-          if (info.name.split(".")[1] === "css") {
+          const extType = info.name.split(".")[1];
+          console.log(extType);
+          if (extType === "css") {
             return "css/style.css";
           }
-          if (info.name.split(".")[1] === "html") {
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            console.log(extType);
+            return "images/[name][extname]";
+          }
+          if (extType === "html") {
             return "[name][extname]";
           }
         },
       },
       input: {
-        index: resolve(__dirname, "./src/index.html"),
+        index: resolve(__dirname, "./index.html"),
       },
     },
   },
